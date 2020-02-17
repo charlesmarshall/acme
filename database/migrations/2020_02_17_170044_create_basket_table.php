dff09fe8c404cf9ba020adf081b0dedf26946d93
Charles Marshall <cm56marshall@googlemail.com>
2020-02-17 22:09:52 +0000

    [acme] basket migration

---
+++ database/migrations/2020_02_17_170044_create_basket_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBasketTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('basket')) {
+            Schema::create('basket', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('basket');
+    }
+}
